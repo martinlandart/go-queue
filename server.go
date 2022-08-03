d43d975cb68e9f457dbca66323382804cc36bbf5
Martin Landart <17906247+martinlandart@users.noreply.github.com>
2022-08-03 19:59:45 -0300

    refactor server

--- server.go
+++ server.go
@@ -17,28 +17,31 @@ type QueueServer struct {
 func (q QueueServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	router := http.NewServeMux()
 
-	router.Handle("/enqueue", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		item, _ := ioutil.ReadAll(r.Body)
-
-		if len(item) == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-		}
-
-		q.queue.Enqueue(string(item))
-
-		w.WriteHeader(http.StatusAccepted)
-	}))
-
-	router.Handle("/dequeue", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		res := q.queue.Dequeue()
-
-		if res == "" {
-			w.WriteHeader(http.StatusNoContent)
-		} else {
-			w.Write([]byte(res))
-			w.WriteHeader(http.StatusOK)
-		}
-	}))
+	router.Handle("/enqueue", http.HandlerFunc(q.enqueueHandler))
+	router.Handle("/dequeue", http.HandlerFunc(q.dequeueHandler))
 
 	router.ServeHTTP(w, r)
+}
+
+func (q QueueServer) enqueueHandler(w http.ResponseWriter, r *http.Request) {
+	item, _ := ioutil.ReadAll(r.Body)
+
+	if len(item) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	q.queue.Enqueue(string(item))
+
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (q QueueServer) dequeueHandler(w http.ResponseWriter, r *http.Request) {
+	res := q.queue.Dequeue()
+
+	if res == "" {
+		w.WriteHeader(http.StatusNoContent)
+	} else {
+		w.Write([]byte(res))
+		w.WriteHeader(http.StatusOK)
+	}
 }
